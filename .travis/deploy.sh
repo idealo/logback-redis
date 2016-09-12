7938a2195ce3c1993995f816a53190aec1704f88
lars.gohlke <lars.gohlke@idealo.de>
2016-09-12 09:37:49 +0200

    moves deployment into the case when a git tag is detected

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-    shred -v ~/.gnupg/*
-    rm -rf ~/.gnupg
-fi
-
-source .travis/gpg.sh
-
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG -Prelease 1>/dev/null 2>/dev/null
+
+    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+
+    source .travis/gpg.sh
+
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease
+
+    if [ ! -z "$TRAVIS" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
-fi
-
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease
-
-
-if [ ! -z "$TRAVIS" ]; then
-    shred -v ~/.gnupg/*
-    rm -rf ~/.gnupg
 fi
