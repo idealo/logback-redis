30541936ecdc0db4543e81b1201e44de6ce0a852
Richard Remus <msg.remus@gmail.com>
2016-09-12 16:21:39 +0200

    trigger gpg script only if tagged

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,19 +2,18 @@
 
 set -e
 
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis/gpg.sh
-
 # only do deployment, when travis detects a new tag
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
+    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+
+    source .travis/gpg.sh
 
     mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease
 
