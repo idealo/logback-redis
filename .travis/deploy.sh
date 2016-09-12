757ec3cb1f52c0d175ea10eeb270703195eed496
lars.gohlke <lars.gohlke@idealo.de>
2016-09-12 11:44:01 +0200

    increase version and show logs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG -Prelease 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
